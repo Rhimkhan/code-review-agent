170935b3e0bcd8e1cded93b9279be7d539743b2e
Rhimkhan <rhim78975730@gmail.com>
2026-08-17 17:18:05 +0530

    feat: add rate limiting middleware to protect public API

---
+++ backend/src/middleware/rate_limiter.py
@@ -0,0 +1,10 @@
+﻿from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+limiter = Limiter(key_func=get_remote_address)
+
+RATE_LIMITS = {
+    'review': '5/minute',
+    'auth': '10/minute',
+    'general': '30/minute'
+}
